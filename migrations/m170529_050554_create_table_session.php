2196b33e85dcac06f4b8a3129fe50e00736ecba4
diazoxide <diazoxide@panteloma>
2019-04-10 22:21:29 +0400

    ...

---
+++ migrations/m170529_050554_create_table_session.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Yordanyan
+ * Date: 10.04
+ * Time: 22:10
+ */
+use yii\db\Migration;
+
+class m170529_050554_create_table_session extends Migration
+{
+    public function up()
+    {
+        $this->createTable('{{%session}}', [
+            'id' => $this->char(64)->notNull(),
+            'expire' => $this->integer(),
+            'data' => $this->binary()
+        ]);
+        $this->addPrimaryKey('pk-id', '{{%session}}', 'id');
+    }
+
+    public function down()
+    {
+        $this->dropTable('{{%session}}');
+    }
+}
