9331914a87da64c34125bc2e6c9a03ea31c4baa5
diazoxide <diazoxide@panteloma>
2019-04-10 16:34:11 +0400

    ...

---
+++ config/db-rename.php
@@ -0,0 +1,18 @@
+<?php
+return [
+    'class' => yii\db\Connection::class,
+    'dsn' => 'mysql:host=localhost;dbname=MY_DB_NAME',
+    'username' => 'MY_DB_USERNAME',
+    'password' => 'MY_DB_PASSWORD',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    'enableSchemaCache' => true,
+    'schemaCacheDuration' => 300,
+    'schemaCache' => 'cache',
+//
+//    'enableQueryCache'=>!YII_DEBUG,
+//    'queryCacheDuration'=>3600,
+
+];
+
